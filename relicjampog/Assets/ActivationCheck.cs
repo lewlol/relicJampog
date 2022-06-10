3b1e8fc74cfd48534d0e36b213f1c55f120ab1f1
lewlol <lewdevbusiness@gmail.com>
2022-06-10 17:51:43 +0100

    WorkingBlockActivation

--- relicjampog/Assets/ActivationCheck.cs
+++ relicjampog/Assets/ActivationCheck.cs
@@ -9,20 +9,34 @@ public class ActivationCheck : MonoBehaviour
     public Sprite deActive;
 
 
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "connection")
+        if(collision.gameObject.tag == "connection" && collision.gameObject.GetComponentInParent<BlockActivated>().isActive == true)
         {
             tile.sprite = active;
             Debug.Log("Connected");
+            gameObject.GetComponentInParent<BlockActivated>().isActive = true;
+        }
+
+        if(collision.gameObject.tag == "power")
+        {
+            gameObject.GetComponentInParent<BlockActivated>().isActive = true;
+            tile.sprite = active;
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "connection")
+        if (collision.gameObject.tag == "connection" && collision.gameObject.GetComponentInParent<BlockActivated>().isActive == true)
         {
             tile.sprite = deActive;
             Debug.Log("Disconnected");
+            gameObject.GetComponentInParent<BlockActivated>().isActive = false;
+        }
+        if (collision.gameObject.tag == "power")
+        {
+            gameObject.GetComponentInParent<BlockActivated>().isActive = false;
+            tile.sprite = deActive;
         }
     }
 }
---
+++ relicjampog/Assets/BlockActivated.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockActivated : MonoBehaviour
+{
+    public bool isActive;
+
+
+    private void Start()
+    {
+        isActive = false;
+    }
+}
