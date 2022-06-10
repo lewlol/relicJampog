e6be23c287b01c36d440f7026bf42af9b84b3bc8
lewlol <lewdevbusiness@gmail.com>
2022-06-11 00:36:08 +0100

    swag

---
+++ relicjampog/Assets/Scripts/TileActiveCheck.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileActiveCheck : MonoBehaviour
+{
+
+    public SpriteRenderer sr;
+    public Sprite active;
+    public Sprite deActive;
+
+
+    public void CheckState()
+    {
+    }
+}
