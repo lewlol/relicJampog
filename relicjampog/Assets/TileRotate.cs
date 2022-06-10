d1e7ea83dd2d983a10c7f2607d25e5ea66180bca
lewlol <lewdevbusiness@gmail.com>
2022-06-10 13:37:26 +0100

    tests

--- relicjampog/Assets/TileRotate.cs
+++ relicjampog/Assets/TileRotate.cs
@@ -4,13 +4,53 @@ using UnityEngine;
 
 public class TileRotate : MonoBehaviour
 {
-    public float currentPos;
-    public float finalpos;
+    public float lPos;
+    public float rPos;
+    private void Start()
+    {
+        lPos = 90;
+        rPos = 90;
+    }
     private void OnMouseOver()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            LeanTween.rotateZ(gameObject, finalpos, 0.5f);
+            RotateLeft();
+            Debug.Log("Left Rotation");
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            RotateRight();
+            Debug.Log("Right Rotation");
+        }
+    }
+
+    public void RotateLeft()
+    {
+        LeanTween.rotateZ(gameObject, lPos, 0.5f);
+        lPos += 90;
+        rPos -= 90;
+        if (rPos < -270)
+        {
+            rPos = 0;
+        }
+        if (lPos > 270)
+        {
+            lPos = 0;
+        }
+    }
+    public void RotateRight()
+    {
+        LeanTween.rotateZ(gameObject, rPos, 0.5f);
+        rPos -= 90;
+        lPos += 90;
+        if (rPos < -270)
+        {
+            rPos = 0;
+        }
+        if (lPos > 270)
+        {
+            lPos = 0;
         }
     }
 }
